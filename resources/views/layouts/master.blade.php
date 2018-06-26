3120b9fc27fe34d4d3c95460b07203bad72cdc3b
SERGEY FOMIN <sergeyfomin@MacBook-Air-SERGEY.local>
2018-06-26 10:56:52 +0300

    dzuba

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -180,8 +180,8 @@
 
 <div align = "center">
 
-	<img src="{{ URL::asset('russia-urugway.jpg') }}" width="400" height="216"></img>
-	<p style="margin-top:5px">"Сегодня!"</p>
+	<img src="{{ URL::asset('urugway.jpg') }}" width="400" height="216"></img>
+	<p style="margin-top:5px">"Плачь, Дзюба, плачь!"</p>
 
 
 </div>
