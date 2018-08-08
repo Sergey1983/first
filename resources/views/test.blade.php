20ae30f72e871a42c64ab231c7f061b211e3f54e
SERGEY FOMIN <sergeyfomin@MacBook-Air-SERGEY.local>
2018-08-08 14:07:44 +0300

    test date jquery

--- resources/views/test.blade.php
+++ resources/views/test.blade.php
@@ -1,17 +1,48 @@
-<?php
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+<script type="text/javascript">
+	
+	$(document).ready(function () {
+
+        $("#button").click(
+            function () {
+
+            $("input[name='hidden_date").attr('value', $("input[name='date']").val());
+            $("input[name='hidden_text").attr('value', $("input[name='text']").val());
+
+
+   			$("div").html($("#form_hidden").serialize());
+
+            }            
+        );
+    });
 
 
 
-phpinfo();
 
-//       setlocale(LC_TIME, 'ru_RU');
+</script>
 
-// dd(setlocale(LC_ALL, ''));
-//       $date_ru = strftime('%d %B %Y', strtotime('2018-01-31'));
+<form id="form_hidden" hidden = "hidden">
 
-//       dd($date_ru);
+	<input type="date" name="hidden_date"/>
+	<input type="text" name="hidden_text"/>
 
-?>
+</form>
+
+<form id="form">
+
+        <input type="date" name="date"/> <label>date</label><br><br>
+        <input type="text" name="text"/> <label>text</label>
+
+</form>
+
+
+<button id="button">print hidden.serialize()</button><br><br>
+<a href="http://www.google.com">link to google.com</a>
+<br>
+<br>
+<div></div>
+
 
 
 
