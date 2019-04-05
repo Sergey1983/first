6d1b225a8d35838979884aec866124268f2f48e1
SERGEY FOMIN <sergey.fomin.vl@gmail.com>
2019-04-05 12:02:19 +0300

    Банки добавлены - 1

--- public/js/create_tour/add_credit.js
+++ public/js/create_tour/add_credit.js
@@ -28,10 +28,21 @@ $(document).ready(function () {
 								'<div class="col-md-8">'+
 
 									'<select class="form-control" id="bank" name="bank">'+
+									
 									'<option selected="selected" disabled="disabled" hidden="hidden" value="">Выберите банк</option>'+
 									'<option value="ООО Хоум кредит энд финанс банк">ООО Хоум кредит энд финанс банк</option>'+
 									'<option value="ООО КБ Ренессанс кредит">ООО КБ Ренессанс кредит</option>'+
 									'<option value="ПАО Почта банк">ПАО Почта банк</option>'+
+									'<option value="АО ОТП Банк">АО ОТП Банк</option>'+
+									'<option value="ООО Русфинанс Банк">ООО Русфинанс Банк</option>'+
+									'<option value="ПАО МТС-Банк">ПАО МТС-Банк</option>'+
+									'<option value="АО Тинькофф Банк">АО Тинькофф Банк</option>'+
+									'<option value="АО Банк Русский Стандарт">АО Банк Русский Стандарт</option>'+
+									'<option value="АО Кредит Европа Банк">АО Кредит Европа Банк</option>'+
+									'<option value="ПАО КБ Восточный">ПАО КБ Восточный</option>'+
+									'<option value="ООО Сетелем Банк">ООО Сетелем Банк</option>'+
+									'<option value="АО АЛЬФА-БАНК">АО АЛЬФА-БАНК</option>'+
+
 									'</select>'+
 
 							 	'</div>'+
